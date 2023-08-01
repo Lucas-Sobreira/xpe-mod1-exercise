c526204ee2a3c8a496c0265cb28672dffd268620
Lucas Andrade Sobreira Santos <83103924+Lucas-Sobreira@users.noreply.github.com>
2023-08-01 20:34:24 -0300

    Update iam.tf
    adicionado emr_default_role, emr_ec2_default_role e alterado o iam:PassRole [id]

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -58,8 +58,8 @@ resource "aws_iam_policy" "lambda" {
         },
         {
           "Action": "iam:PassRole",
-          "Resource": ["arn:aws:iam::127012818163:role/EMR_DefaultRole",
-                       "arn:aws:iam::127012818163:role/EMR_EC2_DefaultRole"],
+          "Resource": ["arn:aws:iam::290799098285:role/EMR_DefaultRole",
+                       "arn:aws:iam::290799098285:role/EMR_EC2_DefaultRole"],
           "Effect": "Allow"
         }
     ]
@@ -71,3 +71,50 @@ resource "aws_iam_role_policy_attachment" "lambda_attach" {
   role       = aws_iam_role.lambda.name
   policy_arn = aws_iam_policy.lambda.arn
 } 
+
+resource "aws_iam_role" "emr_default_role" {
+  name = "EMR_DefaultRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "elasticmapreduce.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "emr_default_role" {
+  role       = aws_iam_role.emr_default_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole"
+}
+
+resource "aws_iam_role" "emr_ec2_default_role" {
+  name = "EMR_EC2_DefaultRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "emr_ec2_default_role" {
+  name = aws_iam_role.emr_ec2_default_role.name
+  role = aws_iam_role.emr_ec2_default_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "emr_ec2_default_role" {
+  role       = aws_iam_role.emr_ec2_default_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role"
+}
